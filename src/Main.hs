cd0f65bf0bb8d39888e42560892b3797e94f80a0
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-07-30 23:52:52 +0200

    first draft of the option system

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,35 @@
 module Main where
 
 import           Parser
+import           Types
 
-import           System.Environment
 
 main :: IO ()
 main = do
     options <- getOptions
-    print options
+
+    let g = opt_global options
+        c = opt_command options
+
+    case c of
+        Generate o  -> runEdenGenerator generate    (g, o)
+        Build o     -> runEdenBuilder   build       (g, o)
+        Test o      -> runEdenTester    test        (g, o)
+        Run o       -> runEdenRunner    run         (g, o)
+        Publish o   -> runEdenPublisher publish     (g, o)
+
+
+generate :: EdenGenerate ()
+generate = return ()
+
+build :: EdenBuild ()
+build = return ()
+
+test :: EdenTest ()
+test = return ()
+
+run :: EdenRun ()
+run = return ()
+
+publish :: EdenPublish ()
+publish = return ()
--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,13 @@
-module Types where
+module Types
+    (
+      module Types
+    , module Control.Monad.Reader
+    , module Control.Monad.IO.Class
+    ) where
+
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Reader   (ReaderT, ask, asks, runReaderT)
+
 
 --[ Command line options ]--
 
@@ -20,11 +29,40 @@ data Command = Generate GenerateOptions
 
 data GenerateOptions = GenerateOptions
   deriving Show
+
 data BuildOptions = BuildOptions
   deriving Show
+
 data TestOptions = TestOptions
   deriving Show
+
 data RunOptions = RunOptions
   deriving Show
+
 data PublishOptions = PublishOptions
   deriving Show
+
+
+
+--[ Monads ]--
+
+type EdenGenerate   = ReaderT (GlobalOptions, GenerateOptions)  IO
+type EdenBuild      = ReaderT (GlobalOptions, BuildOptions)     IO
+type EdenTest       = ReaderT (GlobalOptions, TestOptions)      IO
+type EdenRun        = ReaderT (GlobalOptions, RunOptions)       IO
+type EdenPublish    = ReaderT (GlobalOptions, PublishOptions)   IO
+
+runEdenGenerator :: EdenGenerate a -> (GlobalOptions, GenerateOptions) -> IO a
+runEdenGenerator = runReaderT
+
+runEdenBuilder :: EdenBuild a -> (GlobalOptions, BuildOptions) -> IO a
+runEdenBuilder = runReaderT
+
+runEdenTester :: EdenTest a -> (GlobalOptions, TestOptions) -> IO a
+runEdenTester = runReaderT
+
+runEdenRunner :: EdenRun a -> (GlobalOptions, RunOptions) -> IO a
+runEdenRunner = runReaderT
+
+runEdenPublisher :: EdenPublish a -> (GlobalOptions, PublishOptions) -> IO a
+runEdenPublisher = runReaderT
