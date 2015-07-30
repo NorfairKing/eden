cbc5952f8c9a8235373c2e96e5d50eb225ea0b4e
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-07-30 18:33:51 +0100

    implemented minimal example of optparse

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,54 @@
 module Main where
 
-main = putStrLn "EDEN"
+import Options.Applicative
+
+data Options = Options
+    { opt_quiet :: Bool,
+      opt_command :: Command }
+  deriving Show
+
+data BuildOptions = BuildOptions
+  deriving Show
+data RunOptions = RunOptions
+  deriving Show
+
+data Command = Build BuildOptions
+             | Run RunOptions
+  deriving Show
+
+buildOptions :: Parser Command
+buildOptions = pure $ Build BuildOptions
+
+runOptions :: Parser Command
+runOptions = pure $ Run RunOptions
+
+
+build :: IO ()
+build = putStrLn "build"
+
+run :: IO ()
+run = putStrLn "run"
+
+commandParser :: Parser Command
+commandParser = subparser
+    ( command "build" (info buildOptions idm)
+   <> command "run"   (info runOptions idm))
+
+options :: Parser Options
+options = Options
+     <$> switch
+         ( long "quiet"
+        <> help "Whether to be quiet" )
+     <*> commandParser
+
+eden :: Options -> IO ()
+eden = print
+
+
+main :: IO ()
+main = execParser opts >>= eden
+  where
+    opts = info options
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> header "hello - a test for optparse-applicative" )
