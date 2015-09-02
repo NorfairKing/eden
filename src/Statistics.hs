cbdc500610975f5fd6b40bfb1205b64c2de0929d
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-02 22:53:41 +0200

    added a nice table feature to eden

--- src/Statistics.hs
+++ src/Statistics.hs
@@ -1,5 +1,10 @@
 module Statistics where
 
+import           Text.PrettyPrint.Boxes
+
+import           Data.List              (transpose)
+import           Data.Maybe             (fromJust)
+
 import           Solutions
 import           Types
 
@@ -8,8 +13,32 @@ statistics = runAllStats
 
 runAllStats :: EdenStatistics ()
 runAllStats = do
+    table <- showSolutionStats
+    liftIO $ putStrLn table
     intResults <- mapM runStat allIntStats
     liftIO $ putStrLn $ unlines $ map show intResults
+
+showSolutionStats :: EdenStatistics String
+showSolutionStats = do
+    ls <- languages
+    let titles = char ' ':(map text ls)
+    table <- getTableRows ls
+    let rows = titles:table
+    let table = hsep 2 left (map (vcat center1) (transpose rows))
+    return $ render $ table
+
+getTableRows :: [Language] -> EdenStatistics [[Box]]
+getTableRows ls = do
+    ps <- problems
+    mapM (getTableRow ls) ps
+
+getTableRow :: [Language] -> Problem -> EdenStatistics [Box]
+getTableRow ls p = do
+    bs <- mapM (isSolvedIn p) ls
+    return $ map text $ (show p: map showBool bs)
+  where
+    showBool True  = "[X]"
+    showBool False = "[ ]"
 
 allIntStats :: [StatGenerator Int]
 allIntStats =
